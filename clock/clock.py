6c19d701e446ed59af084303f5a2fe7dbaa73857
Trey Hunner <trey@treyhunner.com>
2017-06-30 21:58:33 -0700

    Add type hints

--- clock/clock.py
+++ clock/clock.py
@@ -1,18 +1,19 @@
-#!/usr/bin/env python
 from datetime import date, datetime, time, timedelta
 
 
-class Clock(object):
-    def __init__(self, hours, minutes):
-        start = datetime.combine(date.today(), time(0, 0))
-        self.dt = start + timedelta(hours=hours, minutes=minutes)
+class Clock:
+    def __init__(self, hours: int, minutes: int) -> None:
+        start: datetime = datetime.combine(date.today(), time(0, 0))
+        self.dt: datetime = start + timedelta(hours=hours, minutes=minutes)
 
-    def add(self, m):
-        self.dt += timedelta(minutes=m)
+    def add(self, minutes: int) -> str:
+        self.dt += timedelta(minutes=minutes)
         return self.dt.strftime('%H:%M')
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Clock):
+            return NotImplemented
         return (self.dt.time() == other.dt.time())
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.dt.strftime('%H:%M')
