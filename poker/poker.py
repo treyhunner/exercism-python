ec05e857635d6f994232210e350af39a43515316
Trey Hunner <trey@treyhunner.com>
2017-07-01 20:27:51 -0700

    Add type annotations to poker

--- poker/poker.py
+++ poker/poker.py
@@ -1,15 +1,16 @@
-from collections import defaultdict, namedtuple
 from enum import IntEnum
 from functools import total_ordering
 from itertools import chain
+from typing import (Any, Callable, DefaultDict, List, Iterable, NamedTuple,
+                    Optional, Sequence, Tuple, Union)
 
 
-def are_consecutive(numbers):
+def are_consecutive(numbers: Sequence[int]) -> bool:
     """Return True if list represents consecutive numbers."""
     return list(range(numbers[0], numbers[-1]+1)) == numbers
 
 
-def multimax(iterable, key=lambda x: x):
+def multimax(iterable: Iterable, key: Callable = lambda x: x) -> List:
     """Return a list of all maximum values."""
     item_scores = [
         (key(item), item)
@@ -35,28 +36,40 @@ class Categories(IntEnum):
     straight_flush = 9
 
 
+class BaseCard(NamedTuple):
+    rank: str
+    suit: str
+
+
 @total_ordering
-class Card(namedtuple('BaseCard', 'rank suit')):
+class Card(BaseCard):
 
     """Card with rank and suit."""
 
-    VALUES = '23456789TJQKA'
+    VALUES: str = '23456789TJQKA'
     ACE_RANK = 11
 
     @property
-    def numeric_rank(self):
+    def numeric_rank(self) -> int:
         return self.VALUES.index(self.rank) + 2
 
-    def __eq__(self, other):
+    def __eq__(self, other: Any) -> Union[bool, type(NotImplemented)]:
         return self.rank == other.rank
 
-    def __lt__(self, other):
+    def __lt__(self, other: Any) -> Union[bool, type(NotImplemented)]:
         return self.rank < other.rank
 
 
-class Score(namedtuple('BaseScore', 'category cards')):
+class BaseScore(NamedTuple):
+    category: Categories
+    cards: Iterable[Card]
+
+
+class Score(BaseScore):
+
     """Score of a poker hand."""
-    def __new__(cls, category, cards):
+
+    def __new__(cls, category: Categories, cards: Iterable[Card]) -> 'Score':
         cards = list(cards)
         all_are_cards = all(isinstance(c, Card) for c in cards)
         if len(cards) != 5 or not all_are_cards:
@@ -66,16 +79,17 @@ class Score(namedtuple('BaseScore', 'category cards')):
         return super().__new__(cls, category, cards)
 
 
-def group_cards_by_rank(cards):
+def group_cards_by_rank(cards: Iterable[Card]) -> List[List[Card]]:
     """Return a list of card lists grouped by rank."""
-    def len_and_vals(cards): return len(cards), cards
-    cards_by_rank = defaultdict(list)
+    def len_and_vals(cards: List[Card]) -> Tuple[int, List[Card]]:
+        return len(cards), cards
+    cards_by_rank = DefaultDict(list)
     for card in cards:
         cards_by_rank[card.rank].append(card)
     return sorted(cards_by_rank.values(), key=len_and_vals, reverse=True)
 
 
-def get_same_rank_score(cards):
+def get_same_rank_score(cards: Iterable[Card]) -> Score:
     """Return score for full house, 3/4 of a kind, pairs, or high card."""
     rank_groups = group_cards_by_rank(cards)
     cards = chain.from_iterable(rank_groups)
@@ -96,14 +110,14 @@ def get_same_rank_score(cards):
         return Score(Categories.high_card, cards)
 
 
-def get_flush_score(cards):
+def get_flush_score(cards: Iterable[Card]) -> Optional[Score]:
     """Return flush score or None."""
     all_suits_match = len(set(c.suit for c in cards)) == 1
     if all_suits_match:
         return Score(Categories.flush, sorted(cards, reverse=True))
 
 
-def get_straight_score(cards):
+def get_straight_score(cards: Iterable[Card]) -> Optional[Score]:
     """Return straight score or None."""
     cards = sorted(cards, reverse=True)
     values = sorted(card.numeric_rank for card in cards)
@@ -115,7 +129,7 @@ def get_straight_score(cards):
         return Score(Categories.straight, cards)
 
 
-def get_straight_flush_score(cards):
+def get_straight_flush_score(cards: Iterable[Card]) -> Optional[Score]:
     """Return straight flush score or None."""
     flush_score = get_flush_score(cards)
     straight_score = get_straight_score(cards)
@@ -123,7 +137,7 @@ def get_straight_flush_score(cards):
         return Score(Categories.straight_flush, straight_score.cards)
 
 
-def score_hand(hand):
+def score_hand(hand: Tuple[str, str]) -> Score:
     """Return best hand score."""
     cards = [Card(*c) for c in hand]
     scores = [
@@ -139,6 +153,6 @@ def score_hand(hand):
     )
 
 
-def poker(hands):
+def poker(hands: Iterable[Tuple[str, str]]) -> Iterable[Tuple[str, str]]:
     """Return a list of all hands with the maximum score."""
     return multimax(hands, key=score_hand)
